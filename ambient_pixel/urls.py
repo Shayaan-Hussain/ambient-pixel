cdf837c452cc155a41f8b48cf21ab83245c1daec
shayaan <shayaan.hussain2@gmail.com>
2021-07-07 23:03:03 +0530

    Modify Upload Block

--- ambient_pixel/urls.py
+++ ambient_pixel/urls.py
@@ -7,6 +7,7 @@ from images import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('/upload', views.homeUpload, name='homeUpload'),
     path('delete/<image_id>', views.delete, name='delete'),
     path('download/<image_id>', views.download, name='download'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- images/views.py
+++ images/views.py
@@ -4,14 +4,20 @@ from .forms import ImageForm
 from .models import Image
 
 # Create your views here.
-def home(request):
+def homeUpload(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
     form = ImageForm
     images = Image.objects.all()
+    if request.method == 'POST':
+        return render(request, 'homeBase.html', {'images':images})
     return render(request, 'home.html', {'images':images})
+
+def home(request):
+    images = Image.objects.all()
+    return render(request, 'homeBase.html', {'images':images})
 
 def delete(request, image_id):
     item = Image.objects.get(pk=image_id)
